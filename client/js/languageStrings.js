543375e76899482874f5971c4e5d41fb95afa789
Even Westvang <even@bengler.no>
2017-03-27 15:00:18 +0200

    Multiple languages first pass

---
+++ client/js/languageStrings.js
@@ -0,0 +1,20 @@
+
+
+module.exports = {
+  en: {
+    loading: 'Please wait, loading images…',
+    description: ''
+  },
+  no: {
+    loading: 'Vennligst vent, bildene laster…',
+    description: 'Her får du utsikt over hele samlingen i ett bilde. Vi har gruppert kunst med likhetstrekk og lar deg se samlingen fugleperspektiv. Samtidig kan du zoome inn på detaljer i enkeltverk hvis du ønsker. \
+      <h2>Hva er dette?</h2> \
+      Vi spurte oss selv: kan vi trene opp en algoritme til å se likheter i samlingen vår og bruke det til å gi bedre oversikt? \
+      \
+      Ved hjelp av maskinlæring viser vi her algoritmens klassifisering av bildene våre etter likheter i motiv og stil. Vi håper dere vil ha nytte av en ny innfallsvinkel til kunsten på nettsidene våre og at visningen oppfordrer publikum til nye oppdagelsesferder i samlingene.\
+      \
+      Prosjektet er støttet av <a href="">Norsk kulturråd</> og utført av seksjon Digital Samlingsforvaltning på Nasjonalmuseet i samarbeid med <a href="http://www.auduno.com/">Audun Mathias Øygard</a> og <a href="http://bengler.no">Bengler</a>. Tematisk ligger arbeidet mellom konvensjonell kunsthistorietenkning og robotikk: to teoretiske retninger som tradisjonelt sett ligger langt fra hverandre. \
+      \
+      For oss har prosjektet vært et nyttig tilskudd i fagforståelsen mellom maskinlæring og kunsthistorie, men vesentligst er ønsket vårt om å bidra til berikelse, forståelse og spredning av det viktige samlingsarbeidet museene gjør.'
+  }
+}
