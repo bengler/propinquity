84e355b5430e3f0364ab3756abdb3635bd957d06
Even Westvang <even@bengler.no>
2017-03-28 15:10:48 +0200

    Language tweaks

--- client/js/languageStrings.js
+++ client/js/languageStrings.js
@@ -8,8 +8,9 @@ module.exports = {
     description: 'This page gives you a map of the entire collection. Artworks have been grouped by similarity. It works like a map so you can zoom in to look at single works. \
       <h2>What am I looking at?</h2> \
       <p>\
-      We trained an algorithm to identify similarities between works in terms of subject and style. On top we built this interface to display how it groups the images. We hope this will help you in finding new ways of exploring and seeing the collections.\
-      </p><p>\
+      We trained an algorithm to identify similarities. On top we built this interface to display how it groups the images. We hope this will help you in finding new ways of exploring and seeing the collections.\
+      </p>\
+      <p>\
       The project received support from <a href="http://www.kulturradet.no/english">The Arts Council Norway</a> and is a collaboration between the Section for Digital Collection Management at the National Museum, <a href="http://www.auduno.com/">Audun Mathias Øygard</a> and <a href="http://bengler.no">Bengler</a>.\
       </p><p>\
       <a href="https://github.com/bengler/propinquity">GitHub</a>\
@@ -17,15 +18,17 @@ module.exports = {
       '
   },
   no: {
-    loading: 'Vennligst vent, bildene laster…',
-    hidehidemebuttoncopy: 'Greit',
+    loading: 'Hold an, laster bilder…',
+    hidehidemebuttoncopy: 'Jøss',
     what: 'Hva er dette?',
     description: 'Her får du utsikt over hele samlingen i ett bilde. Vi har gruppert kunst med likhetstrekk og lar deg se samlingen fugleperspektiv. Samtidig kan du zoome inn på detaljer i enkeltverk hvis du ønsker. \
       <h2>Hva er dette?</h2> \
+      <p>\
       Vi spurte oss selv: kan vi trene opp en algoritme til å se likheter i samlingen vår og bruke det til å gi bedre oversikt? \
-      \
-      Ved hjelp av maskinlæring viser vi her algoritmens klassifisering av bildene våre etter likheter i motiv og stil. Vi håper dere vil ha nytte av en ny innfallsvinkel til kunsten på nettsidene våre og at visningen oppfordrer publikum til nye oppdagelsesferder i samlingene.\
-      \
-      Prosjektet er støttet av <a href="">Norsk kulturråd</> og utført av seksjon Digital Samlingsforvaltning på Nasjonalmuseet i samarbeid med <a href="http://www.auduno.com/">Audun Mathias Øygard</a> og <a href="http://bengler.no">Bengler</a>. Tematisk ligger arbeidet mellom konvensjonell kunsthistorietenkning og robotikk: to teoretiske retninger som tradisjonelt sett ligger langt fra hverandre.'
+      </p><p>\
+      Ved hjelp av maskinlæring viser vi her algoritmens klassifisering av bildene våre etter likheter i motiv og stil. Vi håper dere vil ha nytte av en ny innfallsvinkel til kunsten på nettsidene våre og at visningen oppfordrer til nye oppdagelsesferder i samlingene.\
+      </p><p>\
+      Prosjektet er støttet av <a href="">Norsk kulturråd</a> og utført av seksjon Digital Samlingsforvaltning på Nasjonalmuseet i samarbeid med <a href="http://www.auduno.com/">Audun Mathias Øygard</a> og <a href="http://bengler.no">Bengler</a>.\
+      </p>'
   }
 }
