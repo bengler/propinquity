d48cde075da76ee5dd531bee4e40bdb2f5f89092
Even Westvang <even@bengler.no>
2017-05-03 14:24:34 +0200

    Tweak style and copy

--- client/js/languageStrings.js
+++ client/js/languageStrings.js
@@ -5,10 +5,10 @@ module.exports = {
     loading: 'Please wait, loading images…',
     what: 'What\'s this?',
     hidehidemebuttoncopy: 'OK',
-    description: 'This page gives you an overview of the entire collection. Artworks have been grouped by similarity. It works just like a map so you can zoom in to look at single works. \
+    description: '<p>This page gives you an overview of the entire collection. Artworks have been grouped by similarity. It works just like a map so you can zoom in to look at single works.</p> \
       <h2>What am I looking at?</h2> \
       <p>\
-      We trained a machine learning algorithm to identify similarities in works based on attributes like style and subject. On top we built this interface to show how it groups the works. We hope this will help in finding new ways of exploring the collections.\
+      We trained a machine learning algorithm to identify similarities based on attributes like style and subject. On top we built this interface to show how it groups the works. We hope it will be an inspiring new way to explore the collection.\
       </p>\
       <p>\
       The project received support from <a href="http://www.kulturradet.no/english">The Arts Council Norway</a> and is a collaboration between the Section for Digital Collection Management at the National Museum, <a href="http://www.auduno.com/">Audun Mathias Øygard</a> and <a href="http://bengler.no">Bengler</a>.\
@@ -21,7 +21,7 @@ module.exports = {
     loading: 'Vennligst vent, laster bilder…',
     hidehidemebuttoncopy: 'Greit',
     what: 'Hva er dette?',
-    description: 'Her får du utsikt over hele samlingen i ett bilde. Vi har gruppert kunst med likhetstrekk og lar deg se samlingen fugleperspektiv. Samtidig kan du zoome inn på detaljer i enkeltverk hvis du ønsker. \
+    description: '<p>Her får du utsikt over hele samlingen i ett bilde. Vi har gruppert kunst med likhetstrekk og lar deg se samlingen fugleperspektiv. Samtidig kan du zoome inn på detaljer i enkeltverk hvis du ønsker.</p>\
       <h2>Hva er dette?</h2> \
       <p>\
       Vi spurte oss selv: kan vi trene opp en algoritme til å se likheter i samlingen vår og bruke det til å gi bedre oversikt? \
@@ -29,6 +29,7 @@ module.exports = {
       Ved hjelp av maskinlæring viser vi her algoritmens organisering av bildene våre etter likheter i motiv og stil. Vi håper dere vil ha nytte av en ny innfallsvinkel til kunsten på nettsidene våre og at visningen oppfordrer til nye oppdagelsesferder i samlingene.\
       </p><p>\
       Prosjektet er støttet av <a href="">Norsk kulturråd</a> og utført av seksjon Digital Samlingsforvaltning på Nasjonalmuseet i samarbeid med <a href="http://www.auduno.com/">Audun Mathias Øygard</a> og <a href="http://bengler.no">Bengler</a>.\
+      <small><a href="https://github.com/bengler/propinquity">Ta en titt på kildekoden</a></small>\
       </p>'
   }
 }
